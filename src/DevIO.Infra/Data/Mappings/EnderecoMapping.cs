150cd5591d7e77117e139f5865bec4d6b42d34d7
thalesaugustodias <thalesdias97@gmail.com>
2023-05-01 23:20:55 -0300

    realizando o mapeamento dos dados com fluet api.

---
+++ src/DevIO.Infra/Data/Mappings/EnderecoMapping.cs
@@ -0,0 +1,43 @@
+﻿using DevIO.Business.Models.Fornecedores;
+using System.Data.Entity.ModelConfiguration;
+
+namespace DevIO.Infra.Data.Mappings
+{
+    public class EnderecoMapping : EntityTypeConfiguration<Endereco>
+    {
+        public EnderecoMapping()
+        {
+            HasKey(e => e.Id);
+
+            Property(e => e.Logradouro)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            Property(e => e.Numero)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            Property(e => e.Cep)
+                .IsRequired()
+                .HasMaxLength(8)
+                .IsFixedLength();
+
+            Property(e => e.Complemento)                
+                .HasMaxLength(250);
+
+            Property(e => e.Bairro)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            Property(e => e.Cidade)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            Property(e => e.Estado)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            ToTable("Enderecos");
+        }
+    }
+}
---
+++ src/DevIO.Infra/Data/Mappings/ProdutoMapping.cs
@@ -0,0 +1,32 @@
+﻿using DevIO.Business.Models.Produtos;
+using System.Data.Entity.ModelConfiguration;
+
+
+namespace DevIO.Infra.Data.Mappings
+{
+    public class ProdutoMapping : EntityTypeConfiguration<Produto>
+    {
+        public ProdutoMapping()
+        {
+            HasKey(p => p.Id);
+
+            Property(p => p.Nome)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            Property(p => p.Descricao)
+                .IsRequired()
+                .HasMaxLength(1000);
+
+            Property(p => p.Imagem)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            HasRequired(p => p.Fornecedor)
+                .WithMany(f => f.Produtos)
+                .HasForeignKey(p => p.FornecedorId);
+
+            ToTable("Produtos");
+        }
+    }
+}
