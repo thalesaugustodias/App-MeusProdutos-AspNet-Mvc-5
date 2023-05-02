d34443745cea6ef31c1925b7ef9e0e7fed1b0946
thalesaugustodias <thalesdias97@gmail.com>
2023-05-01 23:21:29 -0300

    adicionando migrations

---
+++ src/DevIO.Infra/Data/Migrations/Configuration.cs
@@ -0,0 +1,14 @@
+﻿using DevIO.Infra.Data.Context;
+using System.Data.Entity.Migrations;
+
+namespace DevIO.Infra.Migrations
+{
+    
+    internal sealed class Configuration : DbMigrationsConfiguration<MeuAppContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+    }
+}
