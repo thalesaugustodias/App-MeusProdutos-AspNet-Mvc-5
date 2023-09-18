55d131ad8de7c18b35e1fb7b96fe02b35c02e88c
thales.cooperx <thales.cooperx@gmail.com>
2023-09-17 22:43:54 -0300

    adicioando repository e controller para testes.

---
+++ DevIO.AppMeusProdutosMvc/Controllers/FornecedorController.cs
@@ -0,0 +1,20 @@
+﻿using DevIO.Business.Models.Fornecedores;
+using DevIO.Business.Models.Fornecedores.Services;
+using System.Web.Mvc;
+
+namespace DevIO.AppMeusProdutosMvc.Controllers
+{
+    public class FornecedorController : Controller
+    {
+        private readonly IFornecedorService _fornecedorService;
+
+        public FornecedorController()
+        {
+            _fornecedorService = new FornecedorService(new FornecedorRepository(), new EnderecoRepository);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
